6700973c0cb38769d907ea23eed99d6be6a3aa4f
ByungYongPark <pby2017@github.com>
2019-02-24 23:55:44 +0900

    유저 데이터베이스 접근 및 트랜잭션 실행 Service 생성

---
+++ src/main/java/service/JoinService.java
@@ -0,0 +1,21 @@
+package service;
+
+import javax.servlet.http.HttpServletRequest;
+
+import model.Users;
+import repository.UsersRepository;
+
+public class JoinService {
+	public void joinUser(HttpServletRequest request, UsersRepository usersRepository) {
+		String userId = request.getParameter("user_id");
+		String userPw = request.getParameter("user_password");
+		String userName = request.getParameter("user_name");
+		
+		Users users = new Users();
+		users.setUser_id(userId);
+		users.setUser_pw(userPw);
+		users.setUser_name(userName);
+		
+		usersRepository.save(users);
+	}
+}
