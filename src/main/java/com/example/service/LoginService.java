6d647c8f5ece40ae41b07f4c788547e2281a5907
ByungYongPark <pby2017@github.com>
2019-02-25 23:11:21 +0900

    HttpSession 속성 추가

--- src/main/java/com/example/service/LoginService.java
+++ src/main/java/com/example/service/LoginService.java
@@ -1,5 +1,7 @@
 package com.example.service;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,25 +10,28 @@ import com.example.repository.UsersRepository;
 
 @Service
 public class LoginService {
-	
+
 	@Autowired
 	private UserPasswordHashClass userPasswordHashClass;
-	
+
 	@Autowired
 	private UsersRepository usersRepository;
-	
+
+	@Autowired
+	HttpSession session;
+
 	public String login(String userId, String userPw) {
-		if(userId.equals("") || userPw.equals("")) {
+		if (userId.equals("") || userPw.equals("")) {
 			return "login";
 		}
-		
+
 		String hashedPassword = userPasswordHashClass.getSHA256(userPw);
-		
-		Users users = usersRepository.findByUser_idAndAndUser_pw(userId, hashedPassword);
-		if(users == null) {
+
+		Users users = usersRepository.findByUser_idAndUser_pw(userId, hashedPassword);
+		if (users == null) {
 			return "login";
 		}
-		
+
 		return "index";
 	}
 }
