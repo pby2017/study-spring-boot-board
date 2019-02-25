0f32dacd7f54329c3f4d4146f5f2e386329a7a7b
ByungYongPark <pby2017@github.com>
2019-02-25 22:34:18 +0900

    JPA ORM 쿼리 메소드 추가

--- src/main/java/com/example/repository/UsersRepository.java
+++ src/main/java/com/example/repository/UsersRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.model.Users;
 
 public interface UsersRepository extends JpaRepository<Users, Long>{
-
+	public Users findByUser_idAndAndUser_pw(String userId, String userPw);
 }
