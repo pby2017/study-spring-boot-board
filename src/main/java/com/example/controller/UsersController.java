9beb3eed2cb4f3837f8dd330892c91917f14d9fb
ByungYongPark <pby2017@github.com>
2019-02-25 22:34:55 +0900

    역할별 리팩토링 & return page 수정

--- src/main/java/com/example/controller/UsersController.java
+++ src/main/java/com/example/controller/UsersController.java
@@ -1,29 +1,42 @@
 package com.example.controller;
 
-import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import com.example.repository.UsersRepository;
 import com.example.service.JoinService;
+import com.example.service.LoginService;
 
 @Controller
 public class UsersController {
 
 	@Autowired
-	private UsersRepository usersRepository;
-	
-	@PostMapping(value="/joinRequest")
-	public String joinRequest(HttpServletRequest request) {
-		JoinService joinService = new JoinService();
-		joinService.joinUser(request, usersRepository);
-		return "index";
+	JoinService joinService;
+
+	@Autowired
+	LoginService loginService;
+
+	@PostMapping("/joinRequest")
+	public String joinRequest(@RequestParam Map<String, String> paramMap) {
+		String userId = paramMap.get("user_id");
+		String userPw = paramMap.get("user_pw");
+		String userName = paramMap.get("user_name");
+
+		String page = joinService.joinUser(userId, userPw, userName);
+
+		return page;
 	}
-	
-	@PostMapping(value="/loginRequest")
-	public String loginRequest() {
-		return "index";
+
+	@PostMapping("/loginRequest")
+	public String loginRequest(@RequestParam Map<String, String> paramMap) {
+		String userId = paramMap.get("user_id");
+		String userPw = paramMap.get("user_pw");
+
+		String page = loginService.login(userId, userPw);
+
+		return page;
 	}
 }
